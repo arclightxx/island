33cf91dda5201b1a5b83c91b3c59c02fa64b5bfd
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-08-08 16:36:08 +0500

    Merge Island and Cell classes

--- src/main/java/com/javarush/island/zaveyboroda/gamefield/Island.java
+++ src/main/java/com/javarush/island/zaveyboroda/gamefield/Island.java
@@ -1,7 +1,15 @@
 package com.javarush.island.zaveyboroda.gamefield;
 
+import com.javarush.island.zaveyboroda.entities.Nature;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Island {
     private Island island;
     private Cell[][] cells;
 
+    public static class Cell {
+        HashMap<String, HashSet<Nature>> naturesOnCellAmount;
+    }
 }
