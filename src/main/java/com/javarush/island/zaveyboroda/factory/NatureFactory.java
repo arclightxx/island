9425a8b5e1ffc62a9b9734c1337485c2a440f71b
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-08-08 22:14:58 +0500

    Make generic NatureFactory

--- src/main/java/com/javarush/island/zaveyboroda/factory/NatureFactory.java
+++ src/main/java/com/javarush/island/zaveyboroda/factory/NatureFactory.java
@@ -1,19 +1,21 @@
 package com.javarush.island.zaveyboroda.factory;
 
 import com.javarush.island.zaveyboroda.entities.Nature;
+import com.javarush.island.zaveyboroda.entities.NatureFeatures;
 import com.javarush.island.zaveyboroda.repository.ConstantNatureFeatures;
 import com.javarush.island.zaveyboroda.repository.DataBase;
 
 import java.lang.reflect.Constructor;
 
 public class NatureFactory {
-    public static Nature createNature(Class<? extends Nature> natureClass, DataBase db) {
+
+    public static <T extends NatureFeatures> T createNature(Class<? extends Nature> natureClass, DataBase db) {
         try {
             Constructor<?> natureConstructor = natureClass.getDeclaredConstructor(ConstantNatureFeatures.class);
             String natureClassName = natureClass.getSimpleName();
-            ConstantNatureFeatures animalFeature = db.getConstantNaturesFeaturesMap().get(natureClassName);
+            ConstantNatureFeatures natureFeature = db.getConstantNaturesFeaturesMap().get(natureClassName);
 
-            return (Nature) natureConstructor.newInstance(animalFeature);
+            return (T) natureConstructor.newInstance(natureFeature);
         } catch (Exception e) {
             throw new RuntimeException("Couldn't create animal instance " + natureClass.getSimpleName() + e.getMessage());
         }
