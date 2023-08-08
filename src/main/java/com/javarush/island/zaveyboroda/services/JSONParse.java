81c69d7edaeff94d7f7146e9b1643a7d9dcb3d6b
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-08-08 16:36:32 +0500

    Remove Singleton implementation

--- src/main/java/com/javarush/island/zaveyboroda/services/JSONParse.java
+++ src/main/java/com/javarush/island/zaveyboroda/services/JSONParse.java
@@ -8,21 +8,11 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class JSONParse {
-    private static JSONParse jsonParse;
-
-    private JSONParse() {
+    public JSONParse() {
 
     }
 
-    public static JSONParse getInstance() {
-        if (jsonParse == null) {
-            jsonParse = new JSONParse();
-        }
-
-        return jsonParse;
-    }
-
-    public <T> T parseJSONFile(ObjectMapper mapper, String filePath, TypeReference<T> typeRef) {
+    public static <T> T parseJSONFile(ObjectMapper mapper, String filePath, TypeReference<T> typeRef) {
         File jsonFile = new File(filePath);
 
         try {
