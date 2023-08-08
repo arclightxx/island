8d640933aba3cbd2c405efe58164e26a1c1fbab0
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-08-08 22:16:13 +0500

    Create MainController

---
+++ src/main/java/com/javarush/island/zaveyboroda/controllers/MainController.java
@@ -0,0 +1,22 @@
+package com.javarush.island.zaveyboroda.controllers;
+
+import com.javarush.island.zaveyboroda.repository.DataBase;
+import com.javarush.island.zaveyboroda.view.View;
+
+public class MainController {
+    private DataBase dataBase;
+    private View view;
+
+    public MainController(DataBase dataBase, View view) {
+        this.dataBase = dataBase;
+        this.view = view;
+    }
+
+    public DataBase getDataBase() {
+        return dataBase;
+    }
+
+    public View getView() {
+        return view;
+    }
+}
