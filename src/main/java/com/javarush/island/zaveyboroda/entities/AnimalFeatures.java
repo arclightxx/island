3b2a22fa431489bfc15e134c5792f172be272da2
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-08-09 13:34:43 +0500

    Rename from NatureFeatures to AnimalFeatures

--- src/main/java/com/javarush/island/zaveyboroda/entities/AnimalFeatures.java
+++ src/main/java/com/javarush/island/zaveyboroda/entities/AnimalFeatures.java
@@ -4,49 +4,45 @@ import com.javarush.island.zaveyboroda.annotations.InjectRandomCurrentAge;
 import com.javarush.island.zaveyboroda.annotations.InjectRandomCurrentWeight;
 import com.javarush.island.zaveyboroda.annotations.InjectRandomGender;
 import com.javarush.island.zaveyboroda.annotations.NatureFeaturesFieldAnnotationProcessor;
-import com.javarush.island.zaveyboroda.gamefield.Cell;
+import com.javarush.island.zaveyboroda.gamefield.Island;
 import com.javarush.island.zaveyboroda.repository.ConstantNatureFeatures;
 import com.javarush.island.zaveyboroda.repository.DeadCause;
 import com.javarush.island.zaveyboroda.repository.Gender;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Objects;
 
-public abstract class NatureFeatures implements Animal {
+public abstract class AnimalFeatures implements Nature {
     private boolean isAlive = true;
-    @InjectRandomCurrentWeight(min = 0.0, max = 0.1)
-    private double currentWeight = 0.0;
-    private int currentMove = 0;
+    @InjectRandomCurrentWeight(adultWeightSpread = 0.1, babyWeightSpread = 0.9)
+    private double currentWeight;
+    private int currentMove;
     @InjectRandomCurrentAge(min = 1)
-    private int currentAge = 1;
+    private int currentAge;
     private DeadCause deadCause;
-    private HashMap<String, Integer> preferableFood = new HashMap<>();
     @InjectRandomGender
     private Gender gender = Gender.FEMALE;
-    private Cell currentLocation;
-    private Cell[][] neighbours;
+    private Island.Cell currentLocation;
 
-    public NatureFeatures() {
+    public AnimalFeatures() {
 
     }
 
-    public NatureFeatures(ConstantNatureFeatures animalFeatures) {
-        NatureFeaturesFieldAnnotationProcessor.calculateAndSetAnnotatedFields(this, animalFeatures);
+    public AnimalFeatures(ConstantNatureFeatures animalFeatures, Island.Cell cell, boolean isBaby) {
+        NatureFeaturesFieldAnnotationProcessor.calculateAndSetAnnotatedFields(this, animalFeatures, isBaby);
+        deadCause = DeadCause.ALIVE;
+        currentLocation = cell;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof NatureFeatures that)) return false;
-        return isAlive == that.isAlive && Double.compare(currentWeight, that.currentWeight) == 0 && currentMove == that.currentMove && currentAge == that.currentAge && deadCause == that.deadCause && Objects.equals(preferableFood, that.preferableFood) && gender == that.gender && Objects.equals(currentLocation, that.currentLocation) && Arrays.deepEquals(neighbours, that.neighbours);
+        if (!(o instanceof AnimalFeatures that)) return false;
+        return isAlive == that.isAlive && Double.compare(currentWeight, that.currentWeight) == 0 && currentMove == that.currentMove && currentAge == that.currentAge && deadCause == that.deadCause && gender == that.gender && Objects.equals(currentLocation, that.currentLocation);
     }
 
     @Override
     public int hashCode() {
-        int result = Objects.hash(isAlive, currentWeight, currentMove, currentAge, deadCause, preferableFood, gender, currentLocation);
-        result = 31 * result + Arrays.deepHashCode(neighbours);
-        return result;
+        return Objects.hash(isAlive, currentWeight, currentMove, currentAge, deadCause, gender, currentLocation);
     }
 
     public String toString() {
@@ -96,14 +92,6 @@ public abstract class NatureFeatures implements Animal {
         this.deadCause = deadCause;
     }
 
-    public HashMap<String, Integer> getPreferableFood() {
-        return preferableFood;
-    }
-
-    public void setPreferableFood(HashMap<String, Integer> preferableFood) {
-        this.preferableFood = preferableFood;
-    }
-
     public Gender getGender() {
         return gender;
     }
@@ -112,25 +100,11 @@ public abstract class NatureFeatures implements Animal {
         this.gender = gender;
     }
 
-    public Cell getCurrentLocation() {
+    public Island.Cell getCurrentLocation() {
         return currentLocation;
     }
 
-    public void setCurrentLocation(Cell currentLocation) {
+    public void setCurrentLocation(Island.Cell currentLocation) {
         this.currentLocation = currentLocation;
     }
-
-    public Cell[][] getNeighbours() {
-        return neighbours;
-    }
-
-    public void setNeighbours(Cell[][] neighbours) {
-        this.neighbours = neighbours;
-    }
-
-//    private double getRandomCurrentWeight(ConstantNatureFeatures animalFeatures) {
-//        double randomWeight = animalFeatures.getMAX_WEIGHT() - (animalFeatures.getMAX_WEIGHT() * Math.random() * 0.1);
-//
-//        return Math.round(randomWeight*1000.0)/1000.0;
-//    }
 }
