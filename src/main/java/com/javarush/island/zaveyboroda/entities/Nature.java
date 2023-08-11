1e560eff451d99add17f82fa8f2ec6158d94aef8
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-08-11 22:01:11 +0500

    Add die method and getters for names

--- src/main/java/com/javarush/island/zaveyboroda/entities/Nature.java
+++ src/main/java/com/javarush/island/zaveyboroda/entities/Nature.java
@@ -2,12 +2,16 @@ package com.javarush.island.zaveyboroda.entities;
 
 import com.javarush.island.zaveyboroda.controllers.MainController;
 import com.javarush.island.zaveyboroda.gamefield.Island;
+import com.javarush.island.zaveyboroda.repository.DeadCause;
 
 public interface Nature {
     default void reproduction(Nature father, Nature mother) {
 
     }
     void grow(MainController controller);
+    void die(DeadCause deadCause);
 
-    String getUniqueName();
+    String getUNIQUE_NAME();
+    String getTYPE_NAME();
+    Island.Cell getCurrentLocation();
 }
