6a4c421de62e539777bc6b6c288b28a37cc820f6
Igor Zaveyboroda <zaveyborodaid@gmail.com>
2023-08-15 00:22:05 +0500

    Remove unused imports

--- src/main/java/com/javarush/island/zaveyboroda/entities/animals/predators/Bear.java
+++ src/main/java/com/javarush/island/zaveyboroda/entities/animals/predators/Bear.java
@@ -1,7 +1,6 @@
 package com.javarush.island.zaveyboroda.entities.animals.predators;
 
 import com.javarush.island.zaveyboroda.entities.AnimalFeatures;
-import com.javarush.island.zaveyboroda.entities.Nature;
 import com.javarush.island.zaveyboroda.entities.Predator;
 import com.javarush.island.zaveyboroda.gamefield.Island;
 import com.javarush.island.zaveyboroda.repository.ConstantNatureFeatures;
--- src/main/java/com/javarush/island/zaveyboroda/entities/animals/predators/Eagle.java
+++ src/main/java/com/javarush/island/zaveyboroda/entities/animals/predators/Eagle.java
@@ -3,7 +3,6 @@ package com.javarush.island.zaveyboroda.entities.animals.predators;
 import com.javarush.island.zaveyboroda.entities.AnimalFeatures;
 import com.javarush.island.zaveyboroda.gamefield.Island;
 import com.javarush.island.zaveyboroda.repository.ConstantNatureFeatures;
-import com.javarush.island.zaveyboroda.entities.Nature;
 import com.javarush.island.zaveyboroda.entities.Predator;
 
 public class Eagle extends AnimalFeatures implements Predator {
